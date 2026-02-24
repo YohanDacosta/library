aeeac58880e37dc431ff31e99acd0c5599f32b39
yohanDacosta <yohan.dacosta910@hotmail.com>
2026-02-24 14:41:58 +0100

    Fixed error in the filter categories, the data filtered was empty

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -4,10 +4,12 @@ namespace App\Repository;
 
 use App\Entity\Book;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\Persistence\ManagerRegistry;
 use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @extends ServiceEntityRepository<Book>
@@ -19,12 +21,19 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
-    public function findAllBooks(): Pagerfanta
+    public function findAllBooks(array $categories = []): Pagerfanta
     {
         $query = $this->createQueryBuilder('b')
-            ->orderBy('b.created_at', 'DESC')
-            ->getQuery();
-        return New Pagerfanta(new QueryAdapter($query));
+            ->orderBy('b.created_at', 'DESC');
+
+        if (!empty($categories)) {
+            $arrayCategoriesUuid = array_map(fn($id) => Uuid::fromString($id)->toBinary(), $categories);
+            $query->distinct()
+                ->innerJoin('b.categories', 'c')
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $arrayCategoriesUuid, ArrayParameterType::BINARY);
+        }
+        return new Pagerfanta(new QueryAdapter($query));
     }
 
     public function filterBookByTitle($filter): Pagerfanta
@@ -34,10 +43,10 @@ class BookRepository extends ServiceEntityRepository
                 ->where('b.title LIKE :filter')
                 ->setParameter('filter', "%{$filter}%")
                 ->getQuery();
-            return New Pagerfanta(new QueryAdapter($query));
+            return new Pagerfanta(new QueryAdapter($query));
         }
 
-        return New Pagerfanta(new ArrayAdapter([]));
+        return new Pagerfanta(new ArrayAdapter([]));
     }
 
     //    /**
